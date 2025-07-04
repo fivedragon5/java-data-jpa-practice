e1758b75f7a9b3975838c22546a6c1427ec0e5d7
fad <ojy9406@naver.com>
2025-07-04 21:38:01 +0900

    fix. 불필요한 import 문 제거

--- src/test/java/fivedragons/data/jpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/fivedragons/data/jpa/repository/MemberJpaRepositoryTest.java
@@ -1,7 +1,6 @@
 package fivedragons.data.jpa.repository;
 
 import fivedragons.data.jpa.entity.Member;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
