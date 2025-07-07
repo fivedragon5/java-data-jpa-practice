28037686d8d9072ca7813e3b10f864d681c12bc4
fad <ojy9406@naver.com>
2025-07-07 17:00:49 +0900

    feat. @Query, 값, DTO 조회하기

--- src/test/java/fivedragons/data/jpa/repository/MemberRepositoryTest.java
+++ src/test/java/fivedragons/data/jpa/repository/MemberRepositoryTest.java
@@ -1,6 +1,8 @@
 package fivedragons.data.jpa.repository;
 
+import fivedragons.data.jpa.dto.MemberDto;
 import fivedragons.data.jpa.entity.Member;
+import fivedragons.data.jpa.entity.Team;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +11,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
 class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
+    @Autowired TeamRepository teamRepository;
 
     @Test
     void testMember() {
@@ -79,5 +82,39 @@ class MemberRepositoryTest {
 
         // then
         Assertions.assertEquals(m1, findMemer);
+    }
+
+    @Test
+    void findUsernameList() {
+        // given
+        Member m1 = new Member("AA", 10);
+        Member m2 = new Member("BB", 10);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        // when
+        List<String> result = memberRepository.findUsernameList();
+
+        // then
+        Assertions.assertEquals(result.size(), 2);
+    }
+
+    @Test
+    void findMemberDto() {
+        // given
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+
+        Member m1 = new Member("AA", 10, team);
+        memberRepository.save(m1);
+
+        // when
+        List<MemberDto> result = memberRepository.findMemberDto();
+        MemberDto memberDto = result.get(0);
+
+        // then
+        assertEquals(memberDto.getUsername(), m1.getUsername());
+        assertEquals(memberDto.getId(), m1.getId());
+        assertEquals(memberDto.getTeamName(), team.getName());
     }
 }
