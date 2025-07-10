06ad22822ad384aad3e5bcf057936212006b2f00
fad <ojy9406@naver.com>
2025-07-10 16:53:08 +0900

    feat. @EntityGraph 추가 테스트

--- src/main/java/fivedragons/data/jpa/repository/MemberRepository.java
+++ src/main/java/fivedragons/data/jpa/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import fivedragons.data.jpa.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -48,4 +49,19 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Modifying(clearAutomatically = true)
     @Query(value = "update Member m set m.age = m.age + 1 where m.age >= :age")
     int bulkAgePlus(@Param("age") int age);
+
+    @Query("select m from Member m join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    @Override
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findAll();
+
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m where m.username = :username")
+    List<Member> findEntityGraphByUserName(@Param("username") String username);
 }
--- src/test/java/fivedragons/data/jpa/repository/MemberRepositoryTest.java
+++ src/test/java/fivedragons/data/jpa/repository/MemberRepositoryTest.java
@@ -252,4 +252,63 @@ class MemberRepositoryTest {
         // then
         assertEquals(result, 3);
     }
+
+    @Test
+    void findMemberLazy() {
+        // given
+        // member1 -> teamA | member2 -> teamB
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        // when N + 1 문제
+        // @EntityGraph를 사용하지 않으면, 프록시 객체가 생성되고, 실제 데이터를 가져오기 위해 추가 쿼리가 발생함
+        List<Member> members = memberRepository.findAll();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            // 프록시 객체를 넣어둠
+            System.out.println("member.teamClass1 = " + member.getTeam().getClass());
+            System.out.println("member.getTeam() = " + member.getTeam().getName());
+        }
+
+        // then
+    }
+
+    @Test
+    void findMemberFetchJoin() {
+        // given
+        // member1 -> teamA | member2 -> teamB
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        // when
+        List<Member> members = memberRepository.findMemberFetchJoin();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            // 프록시 객체를 넣어둠
+            System.out.println("member.teamClass1 = " + member.getTeam().getClass());
+            System.out.println("member.getTeam() = " + member.getTeam().getName());
+        }
+
+        // then
+    }
 }
